15c7e2b9636115639fcd4a926c45d349f66b7a73
DmitriNunes <dmitri.nunes@outlook.com>
2020-11-06 11:18:56 -0500

    Inserting ListItems into a list

--- TestRepo.java
+++ TestRepo.java
@@ -21,5 +21,23 @@ public class TestRepo{
 
     public static void main(String[] args){
         System.out.println("Hello Repo!");
+
+        ListItem li1 = new ListItem(0);
+        ListItem head = li1;
+
+        for(int i = 0; i < 10; i++){
+            ListItem li2 = new ListItem(i);
+            li1.next = li2;
+            li1 = li2;
+            System.out.println("Created ListItem "+i);
+            System.out.println("ListItem address "+li1);
+        }
+
+        while(true){
+            System.out.println("ListItem at:"+head+" has data: "+head.data);
+            head = head.next;
+            if(head.next == null) break;
+        }
+        
     }
 }
