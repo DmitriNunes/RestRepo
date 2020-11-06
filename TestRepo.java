1a488f547770b8b2a635eaf0e698f08975a1b0d2
DmitriNunes <dmitri.nunes@outlook.com>
2020-11-06 11:07:14 -0500

    Added new ListItem class

--- TestRepo.java
+++ TestRepo.java
@@ -1,4 +1,23 @@
+
+class ListItem{
+    public int data;
+    public ListItem next;
+
+    public ListItem(int data){
+        this.data = data;
+        this.next = null;
+    }
+
+    public ListItem(int data, ListItem next){
+        this.data = data;
+        this.next = next;
+    }
+}
+
+
 public class TestRepo{
+
+
 
     public static void main(String[] args){
         System.out.println("Hello Repo!");
